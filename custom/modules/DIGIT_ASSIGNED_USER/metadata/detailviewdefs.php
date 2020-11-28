dda1308caaa7f100453d9fd515fb44049eb18100
AlexSid <seedteam@yandex.ru>
2020-11-28 10:57:12 +0300

    изменены карточки ответственного по задачам

---
+++ custom/modules/DIGIT_ASSIGNED_USER/metadata/detailviewdefs.php
@@ -0,0 +1,84 @@
+<?php
+$module_name = 'DIGIT_ASSIGNED_USER';
+$viewdefs [$module_name] = 
+array (
+  'DetailView' => 
+  array (
+    'templateMeta' => 
+    array (
+      'form' => 
+      array (
+        'buttons' => 
+        array (
+          0 => 'EDIT',
+          1 => 'DUPLICATE',
+          2 => 'DELETE',
+          3 => 'FIND_DUPLICATES',
+        ),
+      ),
+      'maxColumns' => '2',
+      'widths' => 
+      array (
+        0 => 
+        array (
+          'label' => '10',
+          'field' => '30',
+        ),
+        1 => 
+        array (
+          'label' => '10',
+          'field' => '30',
+        ),
+      ),
+      'useTabs' => false,
+      'tabDefs' => 
+      array (
+        'DEFAULT' => 
+        array (
+          'newTab' => false,
+          'panelDefault' => 'expanded',
+        ),
+      ),
+    ),
+    'panels' => 
+    array (
+      'default' => 
+      array (
+        0 => 
+        array (
+          0 => 'name',
+        ),
+        1 => 
+        array (
+          0 => 'description',
+        ),
+        2 => 
+        array (
+          0 => 
+          array (
+            'name' => 'typical_responses',
+            'studio' => 'visible',
+            'label' => 'LBL_TYPICAL_RESPONSES',
+          ),
+        ),
+        3 => 
+        array (
+          0 => 
+          array (
+            'name' => 'digit_task_name',
+            'studio' => 'visible',
+            'label' => 'LBL_DIGIT_TASK_NAME',
+          ),
+          1 => 
+          array (
+            'name' => 'executor_name',
+            'studio' => 'visible',
+            'label' => 'LBL_EXECUTOR_NAME',
+          ),
+        ),
+      ),
+    ),
+  ),
+);
+;
+?>
---
+++ custom/modules/DIGIT_ASSIGNED_USER/metadata/editviewdefs.php
@@ -0,0 +1,74 @@
+<?php
+$module_name = 'DIGIT_ASSIGNED_USER';
+$viewdefs [$module_name] = 
+array (
+  'EditView' => 
+  array (
+    'templateMeta' => 
+    array (
+      'maxColumns' => '2',
+      'widths' => 
+      array (
+        0 => 
+        array (
+          'label' => '10',
+          'field' => '30',
+        ),
+        1 => 
+        array (
+          'label' => '10',
+          'field' => '30',
+        ),
+      ),
+      'useTabs' => false,
+      'tabDefs' => 
+      array (
+        'DEFAULT' => 
+        array (
+          'newTab' => false,
+          'panelDefault' => 'expanded',
+        ),
+      ),
+    ),
+    'panels' => 
+    array (
+      'default' => 
+      array (
+        0 => 
+        array (
+          0 => 'name',
+        ),
+        1 => 
+        array (
+          0 => 'description',
+        ),
+        2 => 
+        array (
+          0 => 
+          array (
+            'name' => 'typical_responses',
+            'studio' => 'visible',
+            'label' => 'LBL_TYPICAL_RESPONSES',
+          ),
+        ),
+        3 => 
+        array (
+          0 => 
+          array (
+            'name' => 'digit_task_name',
+            'studio' => 'visible',
+            'label' => 'LBL_DIGIT_TASK_NAME',
+          ),
+          1 => 
+          array (
+            'name' => 'executor_name',
+            'studio' => 'visible',
+            'label' => 'LBL_EXECUTOR_NAME',
+          ),
+        ),
+      ),
+    ),
+  ),
+);
+;
+?>
